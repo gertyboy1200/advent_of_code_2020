4d7381feea4acf85f9272d4c39908855ebf97346
Garrett Kolenbrander <garrettkolenbrander@garretts-mbp.lan>
2021-09-11 13:48:30 -0700

    add file helpers

---
+++ file_helper.py
@@ -0,0 +1,11 @@
+def get_file_contents(file_name):
+    opened_file = open(file_name)
+
+    lines = opened_file.readlines()
+    stripped_lines = []
+    for x in lines:
+        stripped_lines.append(x.strip())
+    return stripped_lines
+
+
+print(get_file_contents('/Users/garrettkolenbrander/Code/world.py'))
